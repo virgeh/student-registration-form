8d5fdc91285c814ad0da270c977bf45ce0edc5db
virgeh <virgehaavasalu@gmail.com>
2024-11-11 12:04:41 +0200

    All tests of the student registration form are attached.

---
+++ src/test/java/com/example/testsDemo/tests/ChromeDriverDemo.java
@@ -0,0 +1,19 @@
+package com.example.testsDemo.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeDriverDemo {
+	public static void main(String[] args) throws InterruptedException{
+
+		System.out.println("Execution after setting ChromeDriver path in System setProperty method");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Virge\\Desktop\\Woman go Tech\\Dishaga kohtumised\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demoqa.com");
+		Thread.sleep(5000);
+		driver.quit();
+		System.out.println("Execution complete");
+
+	}
+
+}
